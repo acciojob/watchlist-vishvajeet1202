f21e92f88290d348f7ecef4f8a5aa319dd25bc2a
Vishvajeet <112777486+vishvajeet1202@users.noreply.github.com>
2023-01-28 20:46:38 +0530

    One

--- src/main/java/com/driver/Application.java
+++ src/main/java/com/driver/Application.java
@@ -1,12 +1,47 @@
 package com.driver;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import java.util.ArrayList;
+import java.util.List;
 
-@SpringBootApplication
-public class Application {
+public class Director {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
-	}
+    private String name;
+    private int numberOfMovies;
+    private double imdbRating;
+
+
+    public Director(){
+
+    }
+
+    public Director(String name, int numberOfMovies, double imdbrating) {
+        this.name = name;
+        this.numberOfMovies = numberOfMovies;
+        this.imdbRating = imdbrating;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getNumberOfMovies() {
+        return numberOfMovies;
+    }
+
+    public void setNumberOfMovies(int numberOfMovies) {
+        this.numberOfMovies = numberOfMovies;
+    }
+
+    public double getImdbRating() {
+        return imdbRating;
+    }
+
+    public void setImdbRating(double imdbrating) {
+        this.imdbRating = imdbrating;
+    }
+
 }
